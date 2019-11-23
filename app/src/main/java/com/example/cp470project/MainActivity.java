8124402725d1bd97603f141699527830d65ec91a
Alejandro <alrolja@gmail.com>
2019-11-23 18:55:52 -0500

    some error checking for inputs and passing values to resultsactivity

--- app/src/main/java/com/example/cp470project/MainActivity.java
+++ app/src/main/java/com/example/cp470project/MainActivity.java
@@ -21,12 +21,22 @@ public class MainActivity extends AppCompatActivity {
     EditText startWithTxt;
     Spinner amortizationYrs;
     Spinner amortizationMonths;
-    Spinner paymentFreq;
-    Spinner termYrs;
-    Spinner prepayFreq;
+    Spinner paymentFreqSpn;
+    Spinner termYrsSpn;
+    Spinner prepayFreqSpn;
     Button calcBtn;
 
     ArrayAdapter<CharSequence> adapter;
+
+    Double mortgageAmount;
+    Double interestRate;
+    Integer amortizationPeriodY;
+    Integer amortizationPeriodM;
+    String paymentFreq;
+    Integer termYrs;
+    Double prepaymentAmount;
+    String prepaymentFreq;
+    Integer startWithPayment;
 
 
     @Override
@@ -34,17 +44,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i(ACTIVITY_NAME, "In onCreate()");
-        mortgageAmountTxt = findViewById(R.id.mortgage_amount);
-        interestRateTxt = findViewById(R.id.interest_rate);
-        prepayAmountTxt = findViewById(R.id.prepay_amount);
-        startWithTxt = findViewById(R.id.start_with);
+        mortgageAmountTxt = findViewById(R.id.mortgage_amount_etext);
+        interestRateTxt = findViewById(R.id.interest_rate_etext);
+        prepayAmountTxt = findViewById(R.id.prepay_amount_etext);
+        startWithTxt = findViewById(R.id.start_with_etext);
 
-        amortizationYrs = findViewById(R.id.amortization_years);
-        amortizationMonths = findViewById(R.id.amortization_months);
-        paymentFreq = findViewById(R.id.payment_freq);
-        termYrs = findViewById(R.id.term_years);
-        prepayFreq = findViewById(R.id.prepay_freq);
-        calcBtn = findViewById(R.id.calculateBtn);
+        amortizationYrs = findViewById(R.id.amortization_years_spinner);
+        amortizationMonths = findViewById(R.id.amortization_months_spinner);
+        paymentFreqSpn = findViewById(R.id.payment_freq_spinner);
+        termYrsSpn = findViewById(R.id.term_years_spinner);
+        prepayFreqSpn = findViewById(R.id.prepay_freq_spinner);
+        calcBtn = findViewById(R.id.calculate_btn);
 
         //set dropdown menu for the amortization period (years)
         adapter = ArrayAdapter.createFromResource(this,
@@ -62,29 +72,117 @@ public class MainActivity extends AppCompatActivity {
         adapter = ArrayAdapter.createFromResource(this,
                 R.array.payment_freq, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        paymentFreq.setAdapter(adapter);
+        paymentFreqSpn.setAdapter(adapter);
 
         //set dropdown menu for the term length (years)
         adapter = ArrayAdapter.createFromResource(this,
                 R.array.term_yrs, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        termYrs.setAdapter(adapter);
+        termYrsSpn.setAdapter(adapter);
 
         //set dropdown menu for the prepayment frequency
         adapter = ArrayAdapter.createFromResource(this,
                 R.array.prepayment_freq, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        prepayFreq.setAdapter(adapter);
+        prepayFreqSpn.setAdapter(adapter);
 
         calcBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // calculate button pressed
-                Intent intent = new Intent(MainActivity.this, ResultsActivity.class);
-                startActivity(intent);
-
+                if (getInputs()) {
+                    Intent intent = new Intent(MainActivity.this, ResultsActivity.class);
+                    intent.putExtra("MORTGAGE_AMOUNT", mortgageAmount);
+                    intent.putExtra("INTEREST_RATE", interestRate);
+                    intent.putExtra("AMORTIZATION_PERIOD_YRS", amortizationPeriodY);
+                    intent.putExtra("AMORTIZATION_PERIOD_MONTHS", amortizationPeriodM);
+                    intent.putExtra("PAYMENT_FREQUENCY", paymentFreq);
+                    intent.putExtra("TERM", termYrs);
+                    intent.putExtra("PREPAYMENT_AMOUNT", prepaymentAmount);
+                    intent.putExtra("PREPAYMENT_FREQ", prepaymentFreq);
+                    intent.putExtra("START_WITH", startWithPayment);
+                    startActivity(intent);
+                }
             }
         });
 
+    }
+
+    boolean getInputs() {
+
+        Boolean valid = true;
+
+        //mortgage amount
+        String mortgageAmountInput = mortgageAmountTxt.getText().toString();
+        if (!mortgageAmountInput.isEmpty()) {
+            try {
+                mortgageAmount = Double.parseDouble(mortgageAmountInput);
+                // it means it is double
+            } catch (Exception e1) {
+                // this means it is not double
+                e1.printStackTrace();
+            }
+        } else { valid = false; }
+
+        //get amortization period yrs
+        Integer amortYPos = amortizationYrs.getSelectedItemPosition();
+        String[] amortizationYValues = getResources().getStringArray(R.array.amortization_periods_yrs_values);
+        amortizationPeriodY = Integer.valueOf(amortizationYValues[amortYPos]);
+
+        //get amortization period months
+        Integer amortMPos = amortizationMonths.getSelectedItemPosition();
+        String[] amortizationMValues = getResources().getStringArray(R.array.amortization_periods_months_values);
+        amortizationPeriodM = Integer.valueOf(amortizationMValues[amortMPos]);
+
+        //payment freq
+        Integer payFreqPos = paymentFreqSpn.getSelectedItemPosition();
+        String[] payFreqValues = getResources().getStringArray(R.array.payment_freq_values);
+        paymentFreq = payFreqValues[payFreqPos];
+
+        //term
+        Integer termPos = termYrsSpn.getSelectedItemPosition();
+        String[] termYrsValues = getResources().getStringArray(R.array.term_yrs_values);
+        termYrs = Integer.valueOf(termYrsValues[termPos]);
+
+        //prepayment freq
+        Integer prepayFreqPos = prepayFreqSpn.getSelectedItemPosition();
+        String[] prepayFreqValues = getResources().getStringArray(R.array.prepayment_freq_values);
+        prepaymentFreq = prepayFreqValues[prepayFreqPos];
+
+        //interest rate
+        String interestRateInput = interestRateTxt.getText().toString();
+        if (!interestRateInput.isEmpty()) {
+            try {
+                interestRate = Double.parseDouble(interestRateInput);
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        } else { valid = false; }
+
+        // prepayment amount
+        String prepaymentAmountInput = interestRateTxt.getText().toString();
+        if (!prepaymentAmountInput.isEmpty()) {
+            try {
+                prepaymentAmount = Double.parseDouble(prepaymentAmountInput);
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        } else { prepaymentAmount = 0.0; }
+
+        //start with payment
+        String startWithPaymentInput = interestRateTxt.getText().toString();
+        if (!startWithPaymentInput.isEmpty()) {
+            try {
+                startWithPayment = Integer.parseInt(startWithPaymentInput);
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        } else { startWithPayment = 1; }
+
+        //
+        valid = true;
+        //
+
+        return valid;
     }
 
     protected void onResume() {
--- app/src/main/java/com/example/cp470project/ResultsActivity.java
+++ app/src/main/java/com/example/cp470project/ResultsActivity.java
@@ -2,17 +2,71 @@ package com.example.cp470project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class ResultsActivity extends AppCompatActivity {
 
-    protected static final String ACTIVITY_NAME =  "MainActivity";
+    protected static final String ACTIVITY_NAME =  "ResultsActivity";
+
+    Button summaryReportBtn;
+
+    Double mortgageAmount;
+    Double interestRate;
+    Integer amortizationPeriodY;
+    Integer amortizationPeriodM;
+    String paymentFreq;
+    Integer termYrs;
+    Double prepaymentAmount;
+    String prepaymentFreq;
+    Integer startWithPayment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+
+        summaryReportBtn = findViewById(R.id.summary_report_btn);
+
+        if (savedInstanceState == null) {
+            Bundle extras = getIntent().getExtras();
+            mortgageAmount = extras.getDouble("MORTGAGE_AMOUNT");
+            interestRate = extras.getDouble("INTEREST_RATE");
+            amortizationPeriodY = extras.getInt("AMORTIZATION_PERIOD_YRS");
+            amortizationPeriodM = extras.getInt("AMORTIZATION_PERIOD_MONTHS");
+            paymentFreq = extras.getString("PAYMENT_FREQUENCY");
+            termYrs = extras.getInt("TERM");
+            prepaymentAmount = extras.getDouble("PREPAYMENT_AMOUNT");
+            prepaymentFreq = extras.getString("PREPAYMENT_FREQ");
+            startWithPayment = extras.getInt("START_WITH");
+        } else {
+            mortgageAmount = (Double) savedInstanceState.getSerializable("MORTGAGE_AMOUNT");
+            interestRate = (Double) savedInstanceState.getSerializable("INTEREST_RATE");
+            amortizationPeriodY = (Integer) savedInstanceState.getSerializable("AMORTIZATION_PERIOD_YRS");
+            amortizationPeriodM = (Integer) savedInstanceState.getSerializable("AMORTIZATION_PERIOD_MONTHS");
+            paymentFreq = (String) savedInstanceState.getSerializable("PAYMENT_FREQUENCY");
+            termYrs = (Integer) savedInstanceState.getSerializable("TERM");
+            prepaymentAmount = (Double) savedInstanceState.getSerializable("PREPAYMENT_AMOUNT");
+            prepaymentFreq = (String) savedInstanceState.getSerializable("PREPAYMENT_FREQ");
+            startWithPayment = (Integer) savedInstanceState.getSerializable("START_WITH");
+        }
+
+        // go to summary report activity
+        summaryReportBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // calculate button pressed
+                Intent intent = new Intent(ResultsActivity.this, SummaryActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void calculations() {
+        // to do
+
     }
 
     protected void onResume() {
