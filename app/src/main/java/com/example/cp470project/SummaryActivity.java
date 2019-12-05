777ef9c97be8532dff81ddc30bb5dd6701acc340
Alejandro <alrolja@gmail.com>
2019-12-04 20:30:00 -0500

    fixed summary table (still not showing right numbers)

--- app/src/main/java/com/example/cp470project/SummaryActivity.java
+++ app/src/main/java/com/example/cp470project/SummaryActivity.java
@@ -12,7 +12,7 @@ import android.widget.TextView;
 
 public class SummaryActivity extends AppCompatActivity {
 
-    private TableLayout mTableLayout;
+    private TableLayout tl;
 
     double principal;          //loan amount
     double paymentFormula;
@@ -39,47 +39,15 @@ public class SummaryActivity extends AppCompatActivity {
             term = (Integer) savedInstanceState.getSerializable("TERM");
         }
 
+        tl = findViewById(R.id.table_layout);
+        TableLayout.LayoutParams tlparams = new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT, TableLayout.LayoutParams.WRAP_CONTENT);
 
-        mTableLayout = findViewById(R.id.table_layout);
-        mTableLayout.setStretchAllColumns(true);
+        Integer totalMonths = term*12;
+        TextView tv;
+        TableRow tr;
+        for(int i = 0; i < totalMonths; i++){
+            tr = (TableRow) getLayoutInflater().inflate(R.layout.summary_table_row, null);
 
-        TextView text_Spacer = null;
-
-        mTableLayout.removeAllViews();
-
-        final TextView period = new TextView(this);
-        period.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT));
-        period.setText("Period");
-
-        final TextView principle_payment = new TextView(this);
-        principle_payment.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT));
-        principle_payment.setText("Principle Payment");
-
-        final TextView int_payment = new TextView(this);
-        int_payment.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT));
-        int_payment.setText("Interest Payment");
-
-        final TextView tot_payment = new TextView(this);
-        tot_payment.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT));
-        tot_payment.setText("Total Payment");
-
-        final TextView loan_bal = new TextView(this);
-        loan_bal.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT));
-        loan_bal.setText("Loan Balance");
-
-        final TableRow tr = new TableRow(this);
-        tr.setId(0);
-
-        TableLayout.LayoutParams trParams = new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT, TableLayout.LayoutParams.WRAP_CONTENT);
-
-        tr.addView(period);
-        tr.addView(principle_payment);
-        tr.addView(int_payment);
-        tr.addView(tot_payment);
-        tr.addView(loan_bal);
-
-
-        for(int i = 0; i < term; i++){
             //formula to calculate paymentFormula
             paymentFormula = (Math.pow((1 + interestRate), term) -1)/
                     (interestRate * Math.pow((1 + interestRate), term));
@@ -91,48 +59,23 @@ public class SummaryActivity extends AppCompatActivity {
             monthlyPrincipal = monthlyPayment - monthlyInterest;
             loanBalance = loanBalance - monthlyPrincipal;
 
-            period.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT));
-            period.setText("Month " + i);
+            tv = tr.findViewById(R.id.tableCell1);
+            tv.setText("Month " + i+1);
 
-            principle_payment.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT));
-            principle_payment.setText("$ " + (monthlyPayment - monthlyInterest));
+            tv = tr.findViewById(R.id.tableCell2);
+            tv.setText("$ " + (monthlyPayment - monthlyInterest));
 
-            int_payment.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT));
-            int_payment.setText("$ " + monthlyInterest);
+            tv = tr.findViewById(R.id.tableCell3);
+            tv.setText("$ " + monthlyInterest);
 
-            tot_payment.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT));
-            tot_payment.setText("$ " + monthlyPayment);
+            tv = tr.findViewById(R.id.tableCell4);
+            tv.setText("$ " + monthlyPayment);
 
-            loan_bal.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT));
-            loan_bal.setText("$ " + loanBalance);
+            tv = tr.findViewById(R.id.tableCell5);
+            tv.setText("$ " + loanBalance);
 
 
-            tr.setId(i + 1);
-
-            if (period.getParent() != null) {
-                ((ViewGroup)period.getParent()).removeView(period);
-            }
-            tr.addView(period);
-
-            if (principle_payment.getParent() != null) {
-                ((ViewGroup)principle_payment.getParent()).removeView(principle_payment);
-            }
-            tr.addView(principle_payment);
-
-            if (int_payment.getParent() != null) {
-                ((ViewGroup)int_payment.getParent()).removeView(int_payment);
-            }
-            tr.addView(int_payment);
-
-            if (tot_payment.getParent() != null) {
-                ((ViewGroup)tot_payment.getParent()).removeView(tot_payment);
-            }
-            tr.addView(tot_payment);
-
-            if (loan_bal.getParent() != null) {
-                ((ViewGroup)loan_bal.getParent()).removeView(loan_bal);
-            }
-            tr.addView(loan_bal);
+            tl.addView(tr, tlparams);
         }
     }
 }
